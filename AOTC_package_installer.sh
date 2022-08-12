42f5525e81b48de59460d78e482092a385eed2bc
Anonymous Online Training Class <110433361+z4lanonymousk@users.noreply.github.com>
2022-08-12 15:08:10 +0630

    Add files via upload

---
+++ AOTC_package_installer.sh
@@ -0,0 +1,298 @@
+#!/bin/bash
+clear
+
+BLACK='\e[30m'
+RED='\e[31m'
+GREEN='\e[92m'
+YELLOW='\e[33m'
+ORANGE='\e[93m'
+BLUE='\e[34m'
+PURPLE='\e[35m'
+CYAN='\e[36m'
+WHITE='\e[37m'
+NC='\e[0m'
+purpal='\033[35m'
+
+clear
+termux-open https://www.facebook.com/AnonymousOnlineTrainingCourse/
+
+clear
+
+echo -e $CYAN "\n▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading ...\n"
+sleep 0.3
+clear
+echo -e $CYAN "\n▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading ...\n"
+sleep 0.3
+clear
+echo -e $CYAN "\n▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒ Loading ...\n"
+sleep 0.3
+clear
+echo -e $CYAN "\n▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒ Loading ...\n"
+sleep 0.3
+clear
+echo -e $CYAN "\n▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒ Loading ...\n"
+sleep 0.3
+clear
+echo -e $CYAN "\n▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒ Loading ...\n"
+sleep 0.3
+clear
+echo -e $CYAN "\n▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒ Loading ...\n"
+sleep 0.3
+clear
+echo -e $CYAN "\n▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒ Loading ...\n"
+sleep 0.3
+clear
+echo -e $CYAN "\n▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒ Loading ...\n"
+sleep 0.3
+clear
+echo -e $CYAN "\n▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒ Loading ...\n"
+sleep 0.1
+clear
+echo -e $CYAN "\n▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒ Loading ...\n"
+sleep 0.1
+clear
+echo -e $CYAN "\n▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒ Loading ...\n"
+sleep 0.1
+clear
+echo -e $CYAN "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒ Loading ...\n"
+sleep 0.1
+clear
+echo -e $CYAN "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒ Loading ...\n"
+sleep 0.1
+clear
+echo -e $CYAN "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ Loading ...\n"
+sleep 0.1
+
+
+clear
+
+#loadimg code end
+
+echo -e "${ORANGE} "
+
+if ! [ -x "$(command -v toilet)" ]; then
+        echo -e "\e[4;34m  Created By \e[1;32mA.O.C \e[0m"
+    else
+        echo -e "\e[1;34mCreated By \e[1;34m"
+        toilet -f mono12 -F border A.O.C
+    fi
+
+echo ""
+
+echo ""
+ echo  -e $CYAN "1.Basic tool install" 
+ echo ""
+ echo  -e $CYAN "2.Follow me" 
+ echo ""
+ 
+ 
+ 
+ #!exit code
+ echo "[0]  Exit " 
+ echo ""
+ echo  -e $BLUE "Choose A Number :  " 
+read -p "" z4l;
+
+if [ $z4l = 1 ] || [ $z4l = 01 ]
+then
+clear
+ apt-get update -yq --silent 
+ apt-get upgrade -y 
+ apt-get install python -y 
+ apt-get install ruby -y 
+ pip install install lolcat 
+ gem install lolcat
+ pkg install update && pkg install upgrade
+ pkg install ncurses-utils -y 
+ pkg install termux-api -y
+ pkg install termux-repo -y
+ pkg install python3 -y
+ pkg install wget -y 
+ pkg install bash -y
+ pkg install openssh -y
+ pkg install git -y
+ pkg install python -y 
+ pkg install pythpn2 -y
+ pip2 install requests -y
+ pkg install zip wget -y 
+ pip2 install mechanize -y
+ pkg install php -y
+ pip2 install lolcat -y
+ apt install zsh -y
+ pkg install nmap -y
+ pkg install cmatrix -y
+ pkg install sl -y
+ pip3 install colorama -y
+ pkg install nano -y
+ pkg install curl -y
+  pkg update
+ pkg upgrade
+ pkg install python
+ pkg install python2
+ pkg install python3
+ pkg install python-pip
+ pkg install wget
+ pkg install fish
+ pkg install ruby
+ pkg install help
+ pkg install git
+ pkg install dnsutils  
+ pkg install php
+ pkg install perl
+ pkg install lua
+ pkg install parallel
+ pkg install nmap
+ pkg install bash
+ pkg install clang
+ pkg install nano
+ pkg install w3m
+ pkg install hydra
+ pkg install figlet
+ pkg install cowsay
+ pkg install curl
+ pkg install tar
+ pkg install zip
+ pkg install unzip
+ pkg install net-tools
+ pkg install tor -y
+ pkg install sudo -y
+ pkg install wireshark
+ pkg install wgetrc
+ pkg install wcalc
+ pkg install openssl
+ pkg install openssl-tool
+ pkg install bmon
+ pkg install vpn
+ pkg install unrar
+ pkg install toilet
+ pkg install proot
+ pkg install net-tools
+ pkg install vim
+ pkg install vim-python
+ pkg install ired
+ pkg install goaccess
+ pkg install golang
+ pkg install tar
+ pkg install kibi
+ pkg install tsu
+ pkg install tmux
+ pkg install mtools
+ pkg install file
+ pkg install vis
+ pkg install pass
+ pkg install pick
+ pkg install chroot
+ termux-chroot
+ pkg install macchanger
+ pkg install ninja
+ pkg install elixir
+ pkg install swift
+ pkg install xmlstarlet
+ pkg install fakeroot
+ pkg install texinfo
+ pkg install netcat
+ pkg install wren
+ pkg install gatling
+ pkg install cvs
+ pkg install ffmpeg
+ pkg install screen 
+ pkg install neofetch 
+ pkg install mariadb
+ pkg install picolisp
+ pkg install toilet
+ pkg install cmatrix
+ pkg install dropbear
+ pkg install openssh
+ pkg install python-pip
+ pip2 install wget
+ pip install bs4
+ pip2 install bs4
+ pip install requests
+ pip2 install requests
+ pip install mechanize
+ pip2 install mechanize
+ pip install php
+ pip2 install php
+ pip install lolcat
+ pip2 install lolcat
+ termux-setup-storage
+ pip install --upgrade pip
+ apt update && apt upgrade
+ pkg install x11-repo
+ pkg install game-repo
+ pkg install root-repo
+ pkg install unstable-repo
+ pkg install science-repo
+ 
+ ############package install  code start
+ ########### Anonymous Online Training Class
+ ##############
+
+
+
+echo -e $CYAN "\n▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading ...\n"
+sleep 0.3
+clear
+echo -e $CYAN "\n▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒ Loading ...\n"
+sleep 0.3
+clear
+echo -e $CYAN "\n▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒ Loading ...\n"
+sleep 0.3
+clear
+echo -e $CYAN "\n▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒ Loading ...\n"
+sleep 0.3
+clear
+echo -e $CYAN "\n▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒ Loading ...\n"
+sleep 0.3
+clear
+echo -e $CYAN "\n▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒ Loading ...\n"
+sleep 0.3
+clear
+echo -e $CYAN "\n▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒ Loading ...\n"
+sleep 0.3
+clear
+echo -e $CYAN "\n▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒ Loading ...\n"
+sleep 0.3
+clear
+echo -e $CYAN "\n▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒ Loading ...\n"
+sleep 0.3
+clear
+echo -e $CYAN "\n▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒ Loading ...\n"
+sleep 0.1
+clear
+echo -e $CYAN "\n▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒ Loading ...\n"
+sleep 0.1
+clear
+echo -e $CYAN "\n▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒ Loading ...\n"
+sleep 0.1
+clear
+echo -e $CYAN "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒ Loading ...\n"
+sleep 0.1
+clear
+echo -e $CYAN "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒ Loading ...\n"
+sleep 0.1
+clear
+echo -e $CYAN "\n▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ Loading ...\n"
+sleep 0.1
+
+
+clear
+
+#loadimg code end
+ 
+ 
+echo -e $RED  " 🌸 Package install finish 🌸 "
+fi
+
+if [ $z4l = 2 ] || [ $z4l = 02 ]
+then
+termux-open https://www.facebook.com/3rror.fb.unlock.report.9.Z4L
+fi
+
+if [ $z4l = 0 ] || [ $z4l = 0 ]
+then
+echo -e $RED "[✘] Thank You use (Code by AOTC) !! [✘] "
+sleep 2 
+exit
+
+fi
